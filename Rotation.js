56fa6d85d30079a8e3149de2c27cc7570a2817d8
yuvashree11 <81632676+yuvashree11@users.noreply.github.com>
2023-06-08 18:22:45 +0530

    Add files via upload

---
+++ Rotation.js
@@ -0,0 +1,91 @@
+//Terrain Rotation
+AFRAME.registerComponent("terrain-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 }    
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRotation < 0.1) {
+          this.data.speedOfRotation += 0.01;
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRotation > -0.1) {
+          this.data.speedOfRotation -= 0.01;
+        }
+      }
+    });
+  },
+  tick: function () {
+    var mapRotation = this.el.getAttribute("rotation");
+
+    mapRotation.y += this.data.speedOfRotation;
+
+    this.el.setAttribute("rotation", {
+      x: mapRotation.x,
+      y: mapRotation.y,
+      z: mapRotation.z
+    });
+  }
+});
+
+//Plane Rotation
+AFRAME.registerComponent("plane-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 },  
+    speedOfAscent: {type: "number", default: 0}  
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+
+      this.data.speedOfRotation = this.el.getAttribute("rotation")
+      this.data.speedOfAscent = this.el.getAttribute("position")
+
+      var planeRotation = this.data.speedOfRotation
+      var planePosition = thi.data.speedOfAscent
+
+      if (e.key === "ArrowRight") {
+        if (planeRotation.x < 10) {
+           planeRotation.x += 0.5
+           this.el.setAttribute("rotation",planeRotation)
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (planeRotation.x > -10) {
+          planeRotation.x -= 0.5
+          this.el.setAttribute("rotation",planeRotation)
+        }
+      }
+
+      if (e.key === "ArrowUp") {
+        if (planeRotation.z < 20) {
+          planeRotation.z = 0.5
+          this.el.setAttribute("rotation",planeRotation)
+        }
+        
+          if (planeRotation.y < 2) {
+            planeRotation.y += 0.01
+            this.el.setAttribute("position",planePosition)
+          }
+        
+      }
+
+      if (e.key === "ArrowDown") {
+        if (planeRotation.z > -10) {
+          planeRotation.z -= 0.5
+          this.el.setAttribute("rotation",planeRotation)
+        }
+      }
+
+      if (planeRotation.y > -2) {
+        planeRotation.y -= 0.01
+        this.el.setAttribute("position",planePosition)
+      }
+    });
+  },
+});
+
+
+
+
